f0f808b23cff9273f49c40268f188723d883aa5e
Alex <hqwa.sm1@mail.ru>
2022-01-23 11:15:40 +0300

    Initial Commit

---
+++ SimpleCalculator/src/main/java/com/example/simplecalculator/Calculator.kt
@@ -0,0 +1,12 @@
+package com.example.simplecalculator
+
+class Calculator {
+
+    companion object {
+        fun add(a: Int, b: Int): Int = a + b
+        fun sub(a: Int, b: Int): Int = a - b
+        fun mul(a: Int, b: Int): Int = a * b
+        fun div(a: Int, b: Int): Any = a / b
+
+    }
+}
